4b038c2f8d3a2a76b74f0c327ab99bb954fe6b2e
Mayra Espinoza <mayespin@pe.ibm.com>
2019-12-07 02:17:31 -0500

    cambio de backend

--- TerraformAzureDevOps/main.tf
+++ TerraformAzureDevOps/main.tf
@@ -1,18 +1,18 @@
 terraform {
   backend "azurerm" {
-    storage_account_name  = "sademo0308201909000310"
-    container_name        = "tstate"
+    storage_account_name  = "saazuredevops03082019103"
+    container_name        = "scazuredevops"
     key                   = "terraformAzure.tfstate"
   }
 }
 
 resource "azurerm_resource_group" "rgazuredevops" {
-  name     = "rgazuredevops"
+  name     = "rgazuredevops-baz"
   location = "eastus"
 }
 
 resource "azurerm_storage_account" "saazuredevops" {
-  name                     = "saazuredevops03082019103"
+  name                     = "saazuredevops03082019103-baz"
   resource_group_name      = azurerm_resource_group.rgazuredevops.name
   location                 = azurerm_resource_group.rgazuredevops.location
   account_tier             = "Standard"
@@ -20,7 +20,7 @@ resource "azurerm_storage_account" "saazuredevops" {
 }
 
 resource "azurerm_storage_container" "scazuredevops" {
-   name = "scazuredevops"
+   name = "scazuredevops-baz"
    resource_group_name = azurerm_resource_group.rgazuredevops.name
    storage_account_name = azurerm_storage_account.saazuredevops.name
    container_access_type = "blob"
